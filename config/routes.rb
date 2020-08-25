5d98e1daca7ae3c732daed4fa22b8c49675146f1
github username <github email address>
2020-08-25 15:37:09 -0700

    create Game resources, controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
     root to: 'sessions#welcome'
 
     resources :users, only: [:create]
+    resources :games
 
     get 'signup', to: 'users#new'
     get 'login', to: 'sessions#new'
