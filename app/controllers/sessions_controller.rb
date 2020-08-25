3e54eb91b699f8a69afb59ff59d8a5f9adc24e60
github username <github email address>
2020-08-25 15:20:42 -0700

    cleanup SessionsController and add #create method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,15 @@
 class SessionsController < ApplicationController
-    def new
-    end
-
     def create
+        @user = User.find_by(username: params[:username])
+
+        if @user && @user.authenticate(params[:password])
+            session[:user_id] = @user.id
+            redirect_to '/'
+        else
+            redirect_to '/login'
+        end
     end
 
     def destroy
-    end
-
-    def login
-    end
-
-    def welcome
     end
 end
