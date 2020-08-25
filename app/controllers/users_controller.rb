a0f780120a770f447bbfaa68d35b86ecec0c5b97
github username <github email address>
2020-08-25 15:13:45 -0700

    add controller logic for new and create actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,21 @@
 class UsersController < ApplicationController
-  def new
-  end
+    def new
+        @user = User.new
+    end
 
-  def create
-  end
+    def create
+        @user = User.create(user_params)
+        if @user
+            session[:user_id] = @user.id
+            redirect_to '/'
+        else
+            redirect_to '/signup'
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :password)
+    end
 end
