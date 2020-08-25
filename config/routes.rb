61f20931d457c68fb7e7b93fc066d93641cea1fe
github username <github email address>
2020-08-25 15:08:01 -0700

    change users/new route to be '/signup'

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 
-    resources :users, only: [:new, :create]
+    resources :users, only: [:create]
 
+    get 'signup', to: 'users#new'
     get 'login', to: 'sessions#new'
     post 'login', to: 'sessions#create'
     get 'logout', to: 'sessions#destroy'
