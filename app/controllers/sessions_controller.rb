ad4253bc99ad6dea9200a9c003c45fbff0fe2283
github username <github email address>
2020-08-25 15:28:41 -0700

    use skip_before_action macro to avoid redirecting necessary pages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
 class SessionsController < ApplicationController
+    skip_before_action :authorized, only: [:new, :create, :welcome]
+
     def create
         @user = User.find_by(username: params[:username])
 
