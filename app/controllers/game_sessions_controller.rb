23aa27dcdffa3dbe174d9a11233ccea1d4a89ea5
github username <github email address>
2020-08-28 13:37:08 -0700

    create game_sessions#index controller logic

--- app/controllers/game_sessions_controller.rb
+++ app/controllers/game_sessions_controller.rb
@@ -1,5 +1,6 @@
 class GameSessionsController < ApplicationController
     def index
+        @game_sessions = current_user.game_sessions.sort_by{ |sess| sess.date }.reverse
     end
         
     def show
