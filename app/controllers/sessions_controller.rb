ead07f1170700945a574afeab57753e116ad0e7d
github username <github email address>
2020-08-29 16:02:09 -0700

    add google_login controller method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,5 @@
 class SessionsController < ApplicationController
-    skip_before_action :authorized, only: [:new, :create, :welcome]
+    skip_before_action :authorized, only: [:new, :create, :welcome, :google_login]
 
     def create
         @user = User.find_by(username: params[:username])
@@ -12,8 +12,20 @@ class SessionsController < ApplicationController
         end
     end
 
+    def google_login
+        @user = User.from_omniauth(auth)
+        session[:user_id] = @user.id
+        redirect_to '/'
+    end
+
     def destroy
         session[:user_id] = nil
         redirect_to '/'
+    end
+
+    private
+
+    def auth
+        request.env['omniauth.auth']
     end
 end
