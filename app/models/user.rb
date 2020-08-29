d53061edfb842ef561ad23fb4ff107c62cb5d5ee
github username <github email address>
2020-08-29 16:02:21 -0700

    add from_omniauth class method

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,14 @@ class User < ApplicationRecord
 
     has_secure_password
     validates :username, uniqueness: true
+
+    def self.from_omniauth(auth)
+        where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
+            user.provider = auth.provider
+            user.uid = auth.uid
+            user.username = auth.info.email
+            user.password = SecureRandom.hex
+            user.save
+        end
+    end
 end
